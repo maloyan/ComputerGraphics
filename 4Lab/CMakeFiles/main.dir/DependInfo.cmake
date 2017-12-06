eb6dd9270c2b320f6844ab929b8cab71c6508663
Narek Maloyan <narek1110@gmail.com>
2017-12-06 21:44:04 +0300

    4Lab Square-Diamond prototype

---
+++ 4Lab/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/narek/ComputerGraphics/4Lab/glad.c" "/home/narek/ComputerGraphics/4Lab/CMakeFiles/main.dir/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GLFW_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "dependencies/include/GLAD"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/narek/ComputerGraphics/4Lab/Camera.cpp" "/home/narek/ComputerGraphics/4Lab/CMakeFiles/main.dir/Camera.cpp.o"
+  "/home/narek/ComputerGraphics/4Lab/ShaderProgram.cpp" "/home/narek/ComputerGraphics/4Lab/CMakeFiles/main.dir/ShaderProgram.cpp.o"
+  "/home/narek/ComputerGraphics/4Lab/main.cpp" "/home/narek/ComputerGraphics/4Lab/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "dependencies/include/GLAD"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
